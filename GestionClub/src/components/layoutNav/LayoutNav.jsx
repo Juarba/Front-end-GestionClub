aa023f2d48fe57461304ac5ccfcdf7293345044b
Matias D'Anunzio <matidanunzio@gmail.com>
2025-04-10 16:55:39 -0300

    Realice el Router de los componentes

--- GestionClub/src/components/layoutNav/LayoutNav.jsx
+++ GestionClub/src/components/layoutNav/LayoutNav.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const LayoutNav = () => {
-  return (
-    <div>LayoutNav</div>
-  )
-}
+    const navigate = useNavigate();
 
-export default LayoutNav
+    const handleMainPage = () => {
+        navigate("/")
+    }
+
+    const handleBookingPage = () => {
+        navigate("/booking")
+    }
+
+    const handleAboutUs = () => {
+        navigate("/aboutUs")
+    }
+
+    const handleLogin = () => {
+        navigate("/login")
+    }
+    return (
+        <Navbar bg="dark" data-bs-theme="dark" expand="lg" fixed="top" className="w-100">
+            <Container fluid>
+                <Navbar.Brand onClick={handleMainPage}>Logo</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link onClick={handleBookingPage}>Reservar cancha</Nav.Link>
+                    <Nav.Link onClick={handleAboutUs}>Sobre nosotros</Nav.Link>
+                </Nav>
+                <Nav className="ms-auto">
+                    <Nav.Link onClick={handleLogin}>Login</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default LayoutNav;
