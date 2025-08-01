e70cbd37608ba3aed6eb812e98120a8a2da83629
Juan Martin Masqueda <j.masqueda@gmail.com>
2025-08-01 12:02:15 -0300

    dp

--- GestionClub/vite.config.js
+++ GestionClub/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import fs from "fs";
 import path from "path";
 export default defineConfig({
-  base: "/Front-end-GestionClub"
+  base: "/Front-end-GestionClub/",
   plugins: [react()],
   server: {
     https: {
